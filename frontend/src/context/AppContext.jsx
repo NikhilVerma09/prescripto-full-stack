a3bc3743f7421ee925a1c101b4a6e2b234962e6f
Nikhil verma <145831800+NikhilVerma09@users.noreply.github.com>
2025-11-05 22:41:18 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
-    const backendUrl = "https://prescripto-doctor-app-backend.onrender.com"
+    const backendUrl = "https://prescripto-full-stack-backend-qw4x.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
